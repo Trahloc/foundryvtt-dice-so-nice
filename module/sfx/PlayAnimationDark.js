38b97d5f54f2bfc92d7c5781cc46c463adde17b2
Mathias Latournerie <jidewe@gmail.com>
2022-01-17 22:30:51 +0100

    Fixed Dark animation when  a glowing target specified in a 3d model

--- module/sfx/PlayAnimationDark.js
+++ module/sfx/PlayAnimationDark.js
@@ -17,10 +17,20 @@ export class PlayAnimationDark extends DiceSFX {
 
     /**@override play */
     async play() {
+        if(!this.dicemesh.material && this.dicemesh.userData.glow){
+            //We check if there's a glow target specified
+            this.dicemesh.traverse(object => {
+                if (object.userData && object.userData.name && object.userData.name === this.dicemesh.userData.glow) this.glowingMesh=object;
+            });
+        } else if(this.dicemesh.material){
+            this.glowingMesh=this.dicemesh;
+        } else {
+            return false;
+        }
         this.clock = new THREE.Clock();
-        this.baseColor = this.dicemesh.material.color.clone();
-        this.baseMaterial = this.dicemesh.material;
-        this.dicemesh.material = this.baseMaterial.clone();
+        this.baseColor = this.glowingMesh.material.color.clone();
+        this.baseMaterial = this.glowingMesh.material;
+        this.glowingMesh.material = this.baseMaterial.clone();
         AudioHelper.play({
 			src: PlayAnimationDark.sound,
             volume: this.volume
@@ -37,14 +47,14 @@ export class PlayAnimationDark extends DiceSFX {
         } else {
             let val = 0.05172144 + 9.269017*x - 26.55545*x**2 + 26.19969*x**3 - 8.977907*x**4;
             val = Math.min(Math.max(val, 0), 1);
-            this.dicemesh.material.color.copy(this.baseColor);
-            this.dicemesh.material.color.lerp(PlayAnimationDark.darkColor, val);
+            this.glowingMesh.material.color.copy(this.baseColor);
+            this.glowingMesh.material.color.lerp(PlayAnimationDark.darkColor, val);
         }
     }
 
     destroy(){
-        let sfxMaterial = this.dicemesh.material;
-        this.dicemesh.material = this.baseMaterial;
+        let sfxMaterial = this.glowingMesh.material;
+        this.glowingMesh.material = this.baseMaterial;
         sfxMaterial.dispose();
         this.destroyed = true;
     }
